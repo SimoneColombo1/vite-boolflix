2f565f82b785129ae8028b0936b34a29d71ea78f
SimoneColombo1 <simcolombo2@gmail.com>
2024-06-14 13:56:36 +0200

    insertia l'api e stampata in pagina

--- boolflix/src/App.vue
+++ boolflix/src/App.vue
@@ -1,11 +1,56 @@
-<script setup>
+<script>
+import AppMain from './components/AppMain.vue';
+import axios from 'axios';
+export default {
+  components:{
+    AppMain
+  },
+  data() {
+    return {
+      films : [],
+    }
+  },
+  methods: {
+     getFilms(){
+      
 
+// Make a request for a user with a given ID
+axios.get('https://api.themoviedb.org/3/search/movie?api_key=91729d2a0cf04e5b98d1a49d7ccbcf73&query=i&include_adult=true&language=it&page=1%27')
+  .then( (response) => {
+    // handle success
+    console.log(response.data.results);
+    this.films = response.data.results
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+     
+     }
+
+  },
+  created() {
+    this.getFilms();
+  },
+}
 
  
 </script>
 
 <template>
- 
+ <main>
+  <ul>
+    <li v-for="(film, index) in films" :key="index">
+{{ film.original_title }}
+ {{ film.title }}
+    {{ film.original_language }}
+    {{ film.vote_average }}
+</li>
+  </ul>
+ </main>
    
 </template>
 
---
+++ boolflix/src/components/AppMain.vue
@@ -0,0 +1,21 @@
+<script>
+export default{
+    data() {
+        return {
+            
+        }
+    },
+    
+    props:{
+        films:{
+            type:Array,
+            reqired:true
+        }
+    }
+}
+</script>
+<template>
+
+</template>
+
+<style></style>
