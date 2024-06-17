10fda4868b4f9af11f8152c0a9b6d41acc999995
SimoneColombo1 <simcolombo2@gmail.com>
2024-06-17 18:10:13 +0200

    inserita la funzione per riconoscere le lingue

--- boolflix/src/components/FilmList.vue
+++ boolflix/src/components/FilmList.vue
@@ -23,6 +23,13 @@ props:{
  {{ film.title }}
  {{ film.original_title }}
     {{ film.original_language }}
+    <span v-if="film.original_language==='it'">italiano</span>
+    <span v-else-if="film.original_language==='en'">inglese</span>
+    <span v-else-if="film.original_language==='es'">spagnolo </span>
+   <span v-else-if="film.original_language==='fr'">francia</span>
+   <span v-else-if="film.original_language==='ja'">Giappone</span>
+   <span v-else="">mondo</span>
+    
     {{ film.vote_average }}
 </li>
   
